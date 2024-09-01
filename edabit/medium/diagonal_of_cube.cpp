53a60963f9bf6733ea723e1875afcb83ba78896f
MisterEdward <edyiacob321@gmail.com>
2024-09-02 01:16:16 +0300

    chore: Add .idea files and cube diagonal calculation code

--- edabit/medium/diagonal_of_cube.cpp
+++ edabit/medium/diagonal_of_cube.cpp
@@ -1,3 +1,19 @@
-//
-// Created by Edward on 02.09.2024.
-//
+#include <iostream>
+#include <cmath>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+double cubeDiagonal(double volume) {
+    return pow(volume, (double)1/3) * sqrt(3);
+}
+
+int main() {
+    double volume;
+    cout << "Introdu volumul cubului: ";
+    cin >> volume;
+
+    cout << endl << cubeDiagonal(volume);
+    return 0;
+}
