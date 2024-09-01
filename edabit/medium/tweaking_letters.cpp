ecd084fe95ffa19340c3b40447de5ddf5c2b498d
MisterEdward <edyiacob321@gmail.com>
2024-09-02 02:13:19 +0300

    chore: Update tweakLetters function to handle negative indices

--- edabit/medium/tweaking_letters.cpp
+++ edabit/medium/tweaking_letters.cpp
@@ -1,25 +1,30 @@
 // Create a function that tweaks letters by one forward (+1) or backwards (-1) according to an array.
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 
+
 string tweakLetters(string s, vector<int> arr) {
-    for(int i : arr) {
-        if(arr[i] == 1)
-            s[i] += 1;
-        else if(arr[i] == -1)
-            s[i] -= 1;
+    for(size_t i = 0; i < arr.size(); ++i) {
+        s[i] += arr[i];
     }
 
     return s;
 }
 
 int main() {
+    srand(time(NULL));
+
     cout << "Introdu un cuvant: ";
     string word; 
     cin >> word;
-    vector<int> arr = {1, 1, 0, -1, -1};
-
+    vector<int> arr(word.size());
+    for(size_t i = 0; i < arr.size(); i++)
+        arr[i] = rand() % 3 - 1;
+    
     cout << endl << tweakLetters(word, arr);
     return 0;
 }
