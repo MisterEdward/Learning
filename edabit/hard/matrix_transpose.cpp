c4e72a0894d5dddfae3b5d8bfbcbdcbe9555d669
MisterEdward <edyiacob321@gmail.com>
2024-09-04 15:23:35 +0300

    chore: Update matrix_transpose.cpp and valid_hex_code.cpp

---
+++ edabit/hard/matrix_transpose.cpp
@@ -0,0 +1,12 @@
+// Create a function that transposes a 2D matrix.
+#include <iostream>
+using namespace std;
+
+vector<vector<int>> transposeMatrix(vector<vector<int>> arr) {
+	
+}
+
+int main() {
+
+    return 0;
+}
--- edabit/hard/valid_hex_code.cpp
+++ edabit/hard/valid_hex_code.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 bool isValidHexCode(string str) {
-	if(str.size() != 6 && str[0] != '#')
+	if(str.size() != 7 || str[0] != '#')
         return false;
     
     for (int i = 1; i < str.size(); i++) {
         if (!((str[i] >= 'a' && str[i] <= 'f') || 
-            (str[i] >= 'A' && str[i] <= 'A') || 
+            (str[i] >= 'A' && str[i] <= 'F') || 
             (str[i] >= '0' && str[i] <= '9'))) 
                 return false;
     }
@@ -16,6 +16,12 @@ bool isValidHexCode(string str) {
 }
 
 int main() {
-    
+    // make a small test for the function
+    string hexCode = "#FF0000";
+    if (isValidHexCode(hexCode)) {
+        cout << "Valid hex code" << endl;
+    } else {
+        cout << "Invalid hex code" << endl;
+    }
     return 0;
 }
