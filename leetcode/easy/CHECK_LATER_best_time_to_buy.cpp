98fddaefb4a24016d3c534c5c1de43baf4a01021
MisterEdward <edyiacob321@gmail.com>
2024-09-12 01:01:07 +0300

    Refactor code to optimize maxProfit function in CHECK_LATER_best_time_to_buy.cpp

---
+++ leetcode/easy/CHECK_LATER_best_time_to_buy.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int maxProfit(vector<int>& prices) {
+    int buy = prices[0];
+    int profit = 0;
+    
+    for (int i = 0; i < prices.size(); i++) {
+        if(prices[i] < buy)
+            buy = prices[i];
+        else if(prices[i] - buy > profit)
+            profit = prices[i] - buy;
+        }
+    return profit;
+    }
+
+int main() {
+
+    return 0;
+}
